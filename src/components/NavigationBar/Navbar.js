9f8d89673ec0fe20a40b62e59d588f18a4b46a78
changeofheart27 <hieudhanu27@gmail.com>
2021-07-26 03:01:56 +0700

    modify url to use online images (can not use static images)

--- src/components/NavigationBar/Navbar.js
+++ src/components/NavigationBar/Navbar.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
-import picture from "../../images/logo.png";
+import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import picture from '../../images/logo.png';
 
 const Navbar = () => {
   return (
     <div className="container d-flex">
       <div className="flex-grow-1">
         <a href="/home" className="navbar-brand">
-          <img src={picture} alt="" />
+          <img src="https://duongnd2016.gitlab.io/img/ocean_logo.png" alt="" />
         </a>
       </div>
 
